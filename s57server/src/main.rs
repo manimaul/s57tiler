8738c19106e7660506259b66c8e9296a4fe4bd51
William Kamp <manimaul@gmail.com>
2020-12-30 05:35:45 -0800

    some logging

--- s57server/src/main.rs
+++ s57server/src/main.rs
@@ -3,7 +3,8 @@ extern crate log;
 
 use std::env;
 use actix_web::{App, HttpServer, web};
-
+use actix_web::middleware::Logger;
+use s57server::handlers;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -14,15 +15,17 @@ async fn main() -> std::io::Result<()> {
     info!("starting s57server");
     HttpServer::new(|| {
         App::new()
-            .service(s57server::handlers::info)
-            .service(s57server::handlers::post_style)
-            .service(s57server::handlers::get_style)
-            .service(s57server::handlers::post_chart)
-            .service(s57server::handlers::get_chart)
-            .service(s57server::handlers::delete_chart)
-            .service(s57server::handlers::post_geojson)
-            .service(s57server::handlers::get_geojson)
-            .service(s57server::handlers::get_tile)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .service(handlers::info)
+            .service(handlers::post_style)
+            .service(handlers::get_style)
+            .service(handlers::post_chart)
+            .service(handlers::get_chart)
+            .service(handlers::delete_chart)
+            .service(handlers::post_geojson)
+            .service(handlers::get_geojson)
+            .service(handlers::get_tile)
             .app_data(
                 web::JsonConfig::default()
                     // increase body payload size to accommodate large geojson
